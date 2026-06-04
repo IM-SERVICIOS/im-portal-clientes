45a8839381b9bdcd5df339d58e28e4e5972510be
IRIS MARTINEZ <im.contable29@gmail.com>
2026-06-03 20:42:38 -0600

    Update Supabase URL and anon key
    ligas a super base

--- js/supabase.js
+++ js/supabase.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = "https://TU_PROYECTO.supabase.co";
-const SUPABASE_ANON_KEY = "TU_ANON_KEY";
+const SUPABASE_URL = "https://kqbimejfubmsffcacnkg.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_3iC4_HFsjA4J-q8IBdC9Ag_sRzjY4Cj";
 
 const supabase = window.supabase.createClient(
     SUPABASE_URL,

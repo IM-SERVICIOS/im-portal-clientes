bacc00e43e9d35344b47c6ca6f436da25f3df1b3
IRIS MARTINEZ <im.contable29@gmail.com>
2026-06-03 21:40:34 -0600

    Fix user loading function to handle errors

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,8 +1,12 @@
 async function cargarUsuario() {
 
     const {
-        data: { user }
+        data: { user },
+        error
     } = await supabaseClient.auth.getUser();
+
+    console.log("USER:", user);
+    console.log("ERROR:", error);
 
     if (!user) {
         window.location.href = "index.html";
@@ -10,6 +14,7 @@ async function cargarUsuario() {
     }
 
     document.getElementById("usuario").innerHTML = `
+        <h2>Bienvenido</h2>
         <p>${user.email}</p>
     `;
 }
